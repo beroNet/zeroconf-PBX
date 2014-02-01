12144200c42bb6e297d602b410a497101ca27d24
Christian Richter <cr@beronet.com>
2014-02-01 21:37:49 +0100

    moved files to template

--- template/www/index.php
+++ template/www/index.php
@@ -1,45 +1,79 @@
 <?php
 
-# Define functions below this line.
-# Define functions above this line.
 
-# Name of your app:
-$app_name = 'example';
+function get_userapp_name () {
 
-# Basepath of your app:
-$base_path = '/apps/' . $app_name;
+	$ret = "Unknown UserAppFS";
 
-# BEGIN Session management #
+	if (($fp = fopen("/apps/asterisk/VERSION", "r"))) {
+		$buf = fread($fp, 1024);
+		fclose($fp);
 
-$redir_login = '/app/berogui/includes/login.php';
+		if (preg_match("/NAME=.+\n/", $buf, $matches)) {
+			$ret = trim(substr($matches[0], strpos($matches[0], '=') + 1), "\"\n");
+		}
+	}
 
-@session_start();
-if (!isset($_SESSION['beroari_time'])) {
-	header('Location:' . $redir_login . '?userapp=' . $app_name);
-	exit();
-} elseif ((isset($_SESSION['beroari_time'])) && (($_SESSION['beroari_time'] + 1200) < time())) {
-	@session_unset();
-	@session_destroy();
-	header('Location:' . $redir_login . '?reason=sess_expd&userapp=' . $app_name);
-	exit();
+	return($ret);
 }
 
-unset($redir_login);
+$userapp_n	= get_userapp_name();
 
-$_SESSION['beroari_time'] = time();
+if ($_GET['action'] == "reload" ) {
+    exec('/apps/asterisk/bin/asterisk -C /apps/asterisk/etc/asterisk/asterisk.conf -rnx "core reload"');
+}
 
-#  END session management  #
 
-echo	"<xml version=\"1.0\" encoding=\"UTF-8\">\n" .
-	"\t<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.1//EN\" \"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd\">\n" .
-	"<html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"en\">\n" .
-	"\t<head>\n" .
-	"\t\t<title>" . $app_name . "</title>\n" .
-	"\t</head>\n" .
-	"\t<body>\n" .
-	"\t\t<h1>" . $app_name . "</h1>\n" .
-	"\t\t<div>Fill me with content and make me pretty!</div>\n" .
-	"\t</body>\n" .
-	"</html>\n";
+exec('/apps/asterisk/bin/asterisk -C /apps/asterisk/etc/asterisk/asterisk.conf -rnx "sip show peers" | sed "s/Dyn Forcerport ACL//" | sed "s/ D //" | sed "s/OK (\(.*\) ms)/OK-(\1_ms)/"  | grep -v "Monitored:" | sed "s/[ ]*/<\/td><td>/g"  | sed "s/^<td>//" | sed "s/^<\/td>//" | sed "s/<td>$//"', $tmppeers);
+$sippeers=implode("<tr></tr>", $tmppeers);
+
+
+$ret			=	"<h1>" . $userapp_n. " </h1>\n
+				<hr noshade/>" .
+                                "<div>Go to: 
+                                <table><tr>
+                                <td><a href=\"/app/berogui/\">berogui</a></td>
+                                <td><a href=\"filemanager.php\">Asterisk Configuration</a></td>
+                                <td><a href=\"index.php?action=reload\">Reload Configuration</a></td>
+                                </tr></table>
+                                </div>\n" .
+
+                                "<h2>Asterisk</h2>" .
+                                "<div>You can use SIP Phones to register at Asterisk with SIP Port 25060.<br>".
+                                "There are 10 SIP Users: Username=10..20 Secret=10..20<br><br>".
+                                "<b>Example SIP Phone Configuration:</b><br>".
+                                "SIP Registrar: ".$_SERVER['SERVER_NAME'].":25060<br>".
+                                "SIP Server: ".$_SERVER['SERVER_NAME'].":25060<br>".
+                                "SIP Proxy: ".$_SERVER['SERVER_NAME'].":25060<br>".
+                                "SIP Server Port: 25060<br>".
+                                "Username=10<br>".
+                                "Secret=10<br>".
+                                "<br>".
+                                "<b>Howto Dial</b><br>".
+                                "Every extensions can reach each other, by just dialing the extensions. <b>Example:</b> Dial 11 to reach Phone 11<br>".
+                                "The beroFix can be reached by prefixing the call with a 0. <b>Example:</b> Dial 01234 to dial 1234 to berofix.<br>".
+                                "<br><br><b>Stauts:</b><br>".
+                                "<table width=100%>".
+                                $sippeers.
+                                "</table>".
+                                "</div>\n";
+
+echo "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n
+	<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.1//EN\" \"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd\">\n
+	<html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"en\">\n
+		<head>\n
+			<link type=\"text/css\" href=\"./include/css/berofog.css\" rel=\"Stylesheet\" />\n
+			<title>" . $userapp_n. " </title>\n
+		</head>\n
+		<body>\n
+			<div class='main'>\n
+				<div class='top'><img src=\"./include/images/bg_top.png\"/></div>\n
+				<div class='left'>\n"
+					. $ret .
+				"</div>\n
+				<div class='bottom'><img src=\"./include/images/bg_bottom.png\"></div>\n
+			</div>\n
+		</body>\n
+	</html>\n";
 
 ?>
